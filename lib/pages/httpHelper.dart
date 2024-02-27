0fda0232d03f7497793366f8c4165c52fdc14018
Daniel Adeleke <danieladeleke05@gmail.com>
2024-02-27 17:52:14 +0100

    Creating a function to get the headlines

--- lib/pages/httpHelper.dart
+++ lib/pages/httpHelper.dart
@@ -2,7 +2,18 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Articles {
-  Future<List<Map>> getArticles() async {
-    List<Map> articles = [];
+  Future<List<Map>> getHeadlines() async {
+    List<Map> headlines = [];
+
+    http.Response response = await http.get(Uri.parse(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=b40c8ff754314508a61f37a86c9e13f2"));
+
+    if (response.statusCode == 200) {
+      String jsonString = response.body;
+      List data = jsonDecode(jsonString);
+      headlines = data.cast<Map>();
+    }
+
+    return headlines;
   }
 }
