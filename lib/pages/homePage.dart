1507f84f042fe27d04eef7bf5f5bb32e9b67086e
Daniel Adeleke <danieladeleke05@gmail.com>
2024-02-27 19:15:51 +0100

    Added a little bit of functionality to the search bar. Will come back to it later

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newapp/pages/searchPage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -40,24 +41,31 @@ class _HomePageState extends State<HomePage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 48,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2), color: Colors.white),
-              child: Row(
-                children: [
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  const Icon(Icons.search),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    hintText,
-                    style: const TextStyle(color: Colors.grey),
-                  )
-                ],
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const SearchPage()));
+              },
+              child: Container(
+                height: 48,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    color: Colors.white),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    const Icon(Icons.search),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      hintText,
+                      style: const TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
               ),
             ),
           )
