ab1490c6143543767992f6710306cf986db93548
Daniel Adeleke <danieladeleke05@gmail.com>
2024-02-27 18:11:42 +0100

    Created a function to get some specific articles from a search on a particular topic. Also imported the relevant packages

--- lib/pages/httpHelper.dart
+++ lib/pages/httpHelper.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class Articles {
   Future<List<Map>> getHeadlines() async {
@@ -15,5 +16,22 @@ class Articles {
     }
 
     return headlines;
+  }
+
+  Future<List<Map>> getSpecificArticles(String searchItem) async {
+    List<Map> specificArticles = [];
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('yyyy-MMMM-dd').format(now);
+
+    http.Response response = await http.get(Uri.parse(
+        "https://newsapi.org/v2/everything?q=$searchItem&from=$formattedDate&sortBy=popularity&apiKey=b40c8ff754314508a61f37a86c9e13f2"));
+
+    if (response.statusCode == 200) {
+      String jsonString = response.body;
+      List data = jsonDecode(jsonString);
+      specificArticles = data.cast<Map>();
+    }
+
+    return specificArticles;
   }
 }
