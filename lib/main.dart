cd79de5cf86cde0dd72b30d64aa393f6f7a0dc0b
Daniel Adeleke <danieladeleke05@gmail.com>
2024-02-27 17:41:54 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:newapp/pages/homePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}
---
+++ lib/pages/httpHelper.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+class Articles {
+  Future<List<Map>> getArticles() async {
+    List<Map> articles = [];
+
+  }
+}
