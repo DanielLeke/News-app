0e4e4d4569a0d51d1312d8ee4695ce46820467f6
Daniel Adeleke <danieladeleke05@gmail.com>
2024-03-01 19:07:37 +0100

    Styling the search page (not done yet)

--- lib/pages/searchPage.dart
+++ lib/pages/searchPage.dart
@@ -8,8 +8,31 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: ListView(
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  color: Colors.white,
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(color: Colors.grey[400]),
+                    ),
+                  ),
+                ),
+              )),
+        ],
+      ),
+    );
   }
 }
